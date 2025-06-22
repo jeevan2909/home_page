52c1f2528a53b50af955889d125a6ac643df1bea
unknown <jeevanvssk314@gmail.com>
2025-06-22 20:37:03 +0530

    Tasks Manager

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 import "./App.css";
 import { AppRoutes } from "./routes/Routes";
 import { TaskProvider } from "./contexts/TaskContext";
+import AuthProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="h-screen  ">
+      <AuthProvider>
       <TaskProvider>
       <AppRoutes />
       </TaskProvider>
+      </AuthProvider>
     
     </div>
   );
--- vite-project/src/routes/Routes.jsx
+++ vite-project/src/routes/Routes.jsx
@@ -3,16 +3,41 @@ import HomePage from '../components/HomePage';
 import Tasks from '../components/Tasks';
 import AddTasks from '../components/AddTasks';
 import {SingleTask} from "../components/SingleTask"
+import Login from '../components/LoginPage';
+import PrivateComponent from '../components/PrivateComponent';
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
      
       <Routes>
-        <Route path="/" element={<HomePage  />} />
-        <Route path="/tasks" element={<Tasks/>}/>
-        <Route path="/add-task" element={<AddTasks/>}/>
-         <Route path="/task/:id" element={<SingleTask/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <PrivateComponent>
+              <HomePage />
+            </PrivateComponent>
+          }
+        />
+        <Route path="/tasks" element={
+          <PrivateComponent>
+          <Tasks/>
+          </PrivateComponent>
+          }
+        />
+        <Route path="/add-task" element={
+          <PrivateComponent>
+          <AddTasks/>
+          </PrivateComponent>
+          }
+        />
+         <Route path="/task/:id" element={
+          <PrivateComponent>
+          <SingleTask/>
+          </PrivateComponent> 
+          }
+        />
 
       </Routes>
     </BrowserRouter>
